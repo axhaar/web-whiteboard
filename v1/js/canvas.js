fd087b81a4b0a15870689cd5f571b11c5fcadc30
axhaar <pranshumishra2001@gmail.com>
2022-05-16 18:23:11 +0530

    canvas working

--- v1/js/canvas.js
+++ v1/js/canvas.js
@@ -2,3 +2,38 @@ let mousePressed = false;
 let lastX, lastY;
 let ctx;
 
+function InitThis()
+{
+    let canvas = document.getElementById("myCanvas");
+    ctx = canvas.getContext("2d");
+    //   ctx.fillStyle = "white";
+    //   ctx.fillRect(0, 0, canvas.width, canvas.height);
+    $('#myCanvas').mousedown(function (e) {
+        mousePressed = true;
+        Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+    });
+
+    $('#myCanvas').mousemove(function (e) {
+        if (mousePressed) {
+            Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+        }
+    });
+}
+
+function Draw(x, y, isDown) {
+    if (isDown) {
+        ctx.beginPath();
+        ctx.strokeStyle = $('#selColor').val();
+        ctx.lineWidth = $('#selWidth').val();
+        ctx.lineJoin = "round";
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(x, y);
+        ctx.closePath();
+        ctx.stroke();
+    }
+    lastX = x; lastY = y;
+}
+
+$(function () {
+    InitThis();
+})
