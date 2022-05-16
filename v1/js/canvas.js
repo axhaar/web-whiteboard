fef64ef05320f0cb43c3caf9bffbebf042406779
axhaar <pranshumishra2001@gmail.com>
2022-05-16 18:28:05 +0530

    working clear button

--- v1/js/canvas.js
+++ v1/js/canvas.js
@@ -44,3 +44,8 @@ function Draw(x, y, isDown) {
 $(function () {
     InitThis();
 })
+
+$('#deleteButton').on('click', function () {
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+});
